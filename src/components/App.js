e5bcb9b8c5b1495a95462c60bd5ac4f511577cc7
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-02 16:38:49 +0300

    Update App component with addings state and change to class, add handle click method

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,33 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  const calculator = {
-    total: '0',
-    next: '0',
-    operation: '',
-  };
-  return (
-    <>
-      <Display result={calculate(calculator, '').total.toString()} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    const calculator = calculate(this.state, buttonName);
+
+    this.setState(
+      calculator,
+    );
+  }
+
+  render() {
+    const { result } = this.state;
+    return (
+      <>
+        <Display result={result.total} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
 }
 
 export default App;
