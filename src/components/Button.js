968dd765f27fd7ca95afd15c3064a064e73dc78d
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-06 10:40:37 +0300

    Add color prop to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import '../styles/Button.css';
 
 function Button(props) {
-  const { name } = props;
+  const { name, color } = props;
   const handleClick = (buttonName) => props.clickHandler(buttonName);
+  const styles = {
+    backgroundColor: color,
+  };
 
   return (
 
     <button
       className={name === '0' ? 'button zero-button' : 'button'}
       type="button"
+      style={styles}
       onClick={() => handleClick(name)}
     >
       {name}
@@ -18,9 +22,14 @@ function Button(props) {
   );
 }
 
+Button.defaultProps = {
+  color: 'orange',
+};
+
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,35 +8,40 @@ function ButtonPanel(props) {
   return (
     <div className="button-panel">
       <div className="first-group group">
-        <Button name="AC" clickHandler={handleClick} />
-        <Button name="+/-" clickHandler={handleClick} />
-        <Button name="%" clickHandler={handleClick} />
+        <Button name="AC" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="+/-" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="%" clickHandler={handleClick} color="#E0E0E0" />
         <Button name="÷" clickHandler={handleClick} />
       </div>
       <div className="second-group group">
-        <Button name="7" clickHandler={handleClick} />
-        <Button name="8" clickHandler={handleClick} />
-        <Button name="9" clickHandler={handleClick} />
+        <Button name="7" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="8" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="9" clickHandler={handleClick} color="#E0E0E0" />
         <Button name="X" clickHandler={handleClick} />
       </div>
       <div className="third-group group">
-        <Button name="4" clickHandler={handleClick} />
-        <Button name="5" clickHandler={handleClick} />
-        <Button name="6" clickHandler={handleClick} />
+        <Button name="4" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="5" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="6" clickHandler={handleClick} color="#E0E0E0" />
         <Button name="-" clickHandler={handleClick} />
       </div>
       <div className="fourth-group group">
-        <Button name="1" clickHandler={handleClick} />
-        <Button name="2" clickHandler={handleClick} />
-        <Button name="3" clickHandler={handleClick} />
+        <Button name="1" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="2" clickHandler={handleClick} color="#E0E0E0" />
+        <Button name="3" clickHandler={handleClick} color="#E0E0E0" />
         <Button name="+" clickHandler={handleClick} />
       </div>
       <div className="fifth-group group">
         <Button
           name="0"
           clickHandler={handleClick}
+          color="#E0E0E0"
         />
-        <Button name="." clickHandler={handleClick} />
+        <Button
+          name="."
+          clickHandler={handleClick}
+          color="#E0E0E0"
+        />
         <Button name="=" clickHandler={handleClick} />
       </div>
     </div>
